9db258dee09caf1c7f70a8ac9dc348f498aebaea
Wanders-Spring-aka-Skit <avasovazik643@gmail.com>
2022-08-25 13:54:36 +0500

    Xz.

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,11 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        String a1 = "a";
+        String b1 = "b";
+        int a = Integer. parseInt(a1);
+        int b = Integer. parseInt(b1);
+        int formula1 = 9 + a - 5 + b + 2 + a1 - 7;
+        System.out.println(formula1);
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,15 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char first = 'c';;
+        char second = 'h';
+        char third = 'a';
+        char fourth = 'r';
+        int c = first;
+        int h = second;
+        int a = third;
+        int r = fourth;
+        System.out.println(c + h + a + r);
+
     }
 }
