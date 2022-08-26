3b36b97991b3f815f371b96e6487dfa5007c4bcc
Wanders-Spring-aka-Skit <avasovazik643@gmail.com>
2022-08-26 12:10:26 +0500

    Fixed.

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -1,12 +1,15 @@
 package lang.print.gaps.task4;
 
+import java.util.Scanner;
+
 public class FormulaCreator {
     public static void main(String[] args) {
-        String a1 = "a";
-        String b1 = "b";
-        int a = Integer. parseInt(a1);
-        int b = Integer. parseInt(b1);
-        int formula1 = 9 + a - 5 + b + 2 + a1 - 7;
-        System.out.println(formula1);
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int formula1 = (9 * a^a) - (5 * b) + 2 + a - 7;
+        int formula2 = (a + b - 4*a*b) / 2;
+        int wholeFormula = formula1 * formula2;
+        System.out.println(wholeFormula);
     }
 }
