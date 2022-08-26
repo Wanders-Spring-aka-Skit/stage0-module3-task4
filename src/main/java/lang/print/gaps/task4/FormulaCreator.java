2826e1f22ec809fe4e72ea89abd321db06655fb1
Wanders-Spring-aka-Skit <avasovazik643@gmail.com>
2022-08-26 12:28:47 +0500

    Fixed 2.0

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -7,9 +7,6 @@ public class FormulaCreator {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
         int b = scanner.nextInt();
-        int formula1 = (9 * a^a) - (5 * b) + 2 + a - 7;
-        int formula2 = (a + b - 4*a*b) / 2;
-        int wholeFormula = formula1 * formula2;
-        System.out.println(wholeFormula);
+        int formula1 = (9 * a^a - 5 * b + 2 + a - 7) * ((a + b - 4*a*b) / 2);
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,15 +2,14 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
-        char first = 'c';;
-        char second = 'h';
+        char first = 'c';
+        char second = 'r';
         char third = 'a';
         char fourth = 'r';
         int c = first;
         int h = second;
         int a = third;
         int r = fourth;
-        System.out.println(c + h + a + r);
-
+        System.out.println(first + second + third + fourth);
     }
 }
